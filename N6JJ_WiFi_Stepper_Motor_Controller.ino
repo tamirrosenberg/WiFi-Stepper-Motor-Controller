b348d6ced67f76f797c9942b89ab37403c99de40
Tamir Rosenberg <68561398+tamirrosenberg@users.noreply.github.com>
2021-06-17 22:51:06 -0700

    Added information on how to add the Blynk library

--- N6JJ_WiFi_Stepper_Motor_Controller.ino
+++ N6JJ_WiFi_Stepper_Motor_Controller.ino
@@ -2,21 +2,38 @@
 Project: Tuning the Magnetic loop capacitor remotely using Wifi
 Details: This project is based on the ESP8266 Micro Controller, DRV8255 stepper motor driver, bipolar stepper motor and the Blynk App.
 I built a Magnetic Loop Antenna, and my idea was to control the tuning capacitor remotely, using Wifi.
+
+The Blynk App
+-------------
 In the Blynk app, I added five virtual buttons:
   V0 - Tune Left
   V1 - Tune Right
   V2 - Fine Tune Left
   V3 - Fine Tune Right
   V4 - Scan
+
+DRV8825
+-------
 The DRV8825 is a smart motor driver that can be used with normal steps and even with micro-steps, and that can be controlled by
 the driver's pins M0, M1, M2. In my case I wanted to use the 1/32 micro-steps, and these are the speeds that you will see in the code.
 More details about this driver can be found here: https://www.pololu.com/product/2133
+
+Arduino IDE
+-----------
+When you are using the Arduino IDE, you will probalby, need to install the Blynk library, so please follow this procedure:
+From the Arduino IDE menu:
+1. Go to "Sketch" --> "Include Library" --> "Manage Libraries"
+2. Search for "Blynk", and install it
+
 Please feel free to change that to your own project use case.
 Please visit my YouTube channel to see this project in action.
+
 Are you allowed to use this code anywhere? Or change the code? - Yes! You are free to do that! Enjoy!
+The best "Thank you" will be to subscribe to my channel and "like" the video - Thanks in advance!
+
 Design: Tamir Rosenberg, N6JJ
 YouTube Channel Name: re: Ham Radio
-YouTube: https://www.youtube.com/channel/UCHImT8lgZAWDnsUao0wHAQQ
+YouTube Channel link: https://www.youtube.com/channel/UCHImT8lgZAWDnsUao0wHAQQ
 **************************************************************************************************************************************/
 
 #include <ESP8266WiFi.h>
